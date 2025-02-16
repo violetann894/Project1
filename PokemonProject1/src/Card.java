e6f4ee2c796ae80272ab3829bbe108e6117c6047
violetann894 <rachelhuss2976@gmail.com>
2025-02-16 02:01:18 -0500

    Added comments

--- PokemonProject1/src/Card.java
+++ PokemonProject1/src/Card.java
@@ -9,13 +9,16 @@ public class Card {
     private Trainer t;
     private Energy e;
 
+    /**
+     * Default constructor for the Card class.
+     */
     public Card(){
         typeOfCard = "None";
     }
 
     /**
-     *
-     * @param p
+     * Constructor for the Card class.
+     * @param p The Pokemon object.
      */
     public Card(Pokemon p){
         typeOfCard = "Pokemon";
@@ -23,8 +26,8 @@ public class Card {
     }
 
     /**
-     *
-     * @param t
+     * Constructor for the Card class.
+     * @param t The Trainer object.
      */
     public Card(Trainer t){
         typeOfCard = "Trainer";
@@ -32,18 +35,26 @@ public class Card {
     }
 
     /**
-     *
-     * @param e
+     * Constructor for the Card class.
+     * @param e The Energy object.
      */
     public Card(Energy e){
         typeOfCard = "Energy";
         this.e = e;
     }
 
+    /**
+     * Getter method for the typeOfCard variable.
+     * @return The type of card in a string variable.
+     */
     public String getTypeOfCard() {
         return typeOfCard;
     }
 
+    /**
+     * Setter method for the typeOfCard variable
+     * @param typeOfCard The new type of card.
+     */
     public void setTypeOfCard(String typeOfCard) {
         this.typeOfCard = typeOfCard;
     }
