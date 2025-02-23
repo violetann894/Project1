f67cfb4937e82ed5d1683a5e1411e0a6e39b32b1
violetann894 <rachelhuss2976@gmail.com>
2025-02-23 02:27:57 -0500

    Added new variables and methods

--- PokemonProject1/src/Player.java
+++ PokemonProject1/src/Player.java
@@ -10,7 +10,130 @@ public class Player {
     private ArrayList<Card> hand;
     private ArrayList<Card> prizeDeck;
     private ArrayList<Card> bench;
-    private Card activeCard;
+    private Card activePokemon;
+    private ArrayList<Card> discardPile;
 
+    /**
+     *
+     */
+    public Player(){
+        deck = new Deck();
+        deck.generateDeck();
+        deck.shuffle();
+    }
+
+    /**
+     *
+     */
+    public void createHand(){
+        for(int i = 0; i < 7; i++){
+            hand.add(deck.pickTopCard());
+        }
+    }
+
+    /**
+     *
+     */
+    public void pickPrizeDeck(){
+        for(int i = 0; i < 6; i++){
+            prizeDeck.add(deck.pickTopCard());
+        }
+    }
+
+    /**
+     * Getter method for the variable deck
+     * @return The current deck of cards
+     */
+    public Deck getDeck() {
+        return deck;
+    }
+
+    /**
+     * Setter method for the deck variable
+     * @param deck The new deck of cards
+     */
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
+    /**
+     * Getter method for the hand variable
+     * @return The current hand of cards
+     */
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    /**
+     * Setter method for the hand variable
+     * @param hand The new hand of cards
+     */
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
+    /**
+     * Getter method for the prizeDeck variable
+     * @return The current prize deck
+     */
+    public ArrayList<Card> getPrizeDeck() {
+        return prizeDeck;
+    }
+
+    /**
+     * Setter method for the prizeDeck variable
+     * @param prizeDeck The new prize deck
+     */
+    public void setPrizeDeck(ArrayList<Card> prizeDeck) {
+        this.prizeDeck = prizeDeck;
+    }
+
+    /**
+     * Getter method for the bench variable
+     * @return The ArrayList of cards in the bench
+     */
+    public ArrayList<Card> getBench() {
+        return bench;
+    }
+
+    /**
+     * Setter method for the bench
+     * @param bench The new ArrayList of cards in the bench
+     */
+    public void setBench(ArrayList<Card> bench) {
+        this.bench = bench;
+    }
+
+    /**
+     * Getter method for the activePokemon variable
+     * @return The current active pokemon
+     */
+    public Card getActivePokemon() {
+        return activePokemon;
+    }
+
+    /**
+     * Setter method for the activePokemon variable
+     * @param activePokemon The new active pokemon
+     */
+    public void setActivePokemon(Card activePokemon) {
+        this.activePokemon = activePokemon;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Card> getDiscardPile() {
+        return discardPile;
+    }
+
+    /**
+     *
+     * @param discardPile
+     */
+    public void setDiscardPile(ArrayList<Card> discardPile) {
+        this.discardPile = discardPile;
+    }
 
 }
--- PokemonProject1/src/PokemonCardGame.java
+++ PokemonProject1/src/PokemonCardGame.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * The PokemonCardGame class is responsible for creating and running a Pokemon TCG game between two players. It holds
@@ -6,6 +7,72 @@ import java.util.ArrayList;
  */
 public class PokemonCardGame {
 
+    private Player player1;
+    private Player player2;
+    private Scanner userInput = new Scanner(System.in);
+    private boolean firstTurnCheck;
+    private String whoGoesFirst;
 
+    /**
+     *
+     */
+    public void startGame(){
+        System.out.println("Starting game . . .");
+
+        player1 = new Player();
+        player2 = new Player();
+
+        player1.createHand();
+        player2.createHand();
+
+        player1.pickPrizeDeck();
+        player2.pickPrizeDeck();
+
+        firstTurnCheck = true;
+    }
+
+    /***
+     *
+     * @return
+     */
+    public String coinFlip(){
+        Random random = new Random();
+
+        int coinFace = random.nextInt(0, 2);
+
+        if(coinFace == 0){
+            return "Heads";
+        }else{
+            return "Tails";
+        }
+    }
+
+    /**
+     *
+     */
+    public void mulligan(){
+
+    }
+
+    /**
+     *
+     */
+    public void player1Turn(){
+
+    }
+
+    /**
+     *
+     */
+    public void player2Turn(){
+
+    }
+
+    /**
+     *
+     */
+    public void runGame(){
+
+    }
 
 }
