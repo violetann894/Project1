dd11b1c418872376e92e38faddc91029c183ca26
violetann894 <rachelhuss2976@gmail.com>
2025-02-22 22:26:12 -0500

    Added comments

--- PokemonProject1/src/PokemonCardGame.java
+++ PokemonProject1/src/PokemonCardGame.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 
+/**
+ * The PokemonCardGame class is responsible for creating and running a Pokemon TCG game between two players. It holds
+ * all the core methods and game state variables.
+ */
 public class PokemonCardGame {
 
     private ArrayList<Card> player1;
