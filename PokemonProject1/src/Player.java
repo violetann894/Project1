a3df5ffc3775ea7112e7513cf78103e479aaa7b9
violetann894 <rachelhuss2976@gmail.com>
2025-02-23 00:03:38 -0500

    Initial commit

---
+++ PokemonProject1/src/Player.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * The Player class handles the state of the player.
+ * @author Rachel Hussmann
+ */
+public class Player {
+
+    private Deck deck;
+    private ArrayList<Card> hand;
+    private ArrayList<Card> prizeDeck;
+    private ArrayList<Card> bench;
+    private Card activeCard;
+
+
+}
