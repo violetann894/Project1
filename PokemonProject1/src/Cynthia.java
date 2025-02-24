92c17239e61729f7b420875734f1952876a98707
violetann894 <rachelhuss2976@gmail.com>
2025-02-23 20:35:45 -0500

    Added card functionality

--- PokemonProject1/src/Cynthia.java
+++ PokemonProject1/src/Cynthia.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * The Cynthia class is a trainer card that can be used in a Pokemon TCG game.
  * @author Rachel Hussmann
@@ -11,8 +13,23 @@ public class Cynthia extends Trainer{
         setNameOfCard("Cynthia");
         setTypeOfTrainerCard("Supporter");
         setTypeOfCard("Trainer");
-        setDescriptionOfCard("Shuffle you hand into your deck. Then, draw 6 cards");
+        setDescriptionOfCard("Shuffle your hand into your deck. Then, draw 6 cards");
         setTrainer(this);
+    }
+
+    public void shuffleAndDraw(Player playerWithCard){
+
+        ArrayList<Card> hand = playerWithCard.getHand();
+        Deck playerDeck = playerWithCard.getDeck();
+
+        playerDeck.returnHandToDeck(hand);
+        hand.clear();
+
+        for(int i = 0; i < 6; i++) {
+            hand.add(playerDeck.pickTopCard());
+        }
+
+        playerWithCard.setHand(hand);
     }
 
 }
