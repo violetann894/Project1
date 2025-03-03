1f304d21a675d56b4b7ecc8e58a57ba38b53c62b
violetann894 <rachelhuss2976@gmail.com>
2025-03-03 00:08:29 -0500

    Renamed PokemonProjectTester to PokemonCardGameTester

---
+++ PokemonProject1/src/PokemonCardGameTester.java
@@ -0,0 +1,10 @@
+/**
+ * The PokemonProjectTester class is responsible for testing the PokemonCardGame class and all related classes.
+ * @author Rachel Hussmann
+ */
+public class PokemonCardGameTester {
+    public static void main(String[] args) {
+        PokemonCardGame pokemonTester = new PokemonCardGame();
+        pokemonTester.startGame();
+    }
+}
