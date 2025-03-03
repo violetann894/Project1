96bdd73f03ae585238a1905a7aa9accba389205d
violetann894 <rachelhuss2976@gmail.com>
2025-03-03 00:20:15 -0500

    Added comments

--- MonteCarloDoorGame/src/Door.java
+++ MonteCarloDoorGame/src/Door.java
@@ -1,16 +1,31 @@
-
+/**
+ * The Door class manages door objects to test the Monty Hall Door Problem.
+ * @author Rachel Hussmann
+ */
 public class Door {
 
     private String behindDoor;
 
+    /**
+     * Default constructor
+     * @param behindDoor A String of what is behind the door
+     */
     public Door(String behindDoor){
         this.behindDoor = behindDoor;
     }
 
+    /**
+     * Getter for the behhindDoor variable
+     * @return The string of what is behind the door
+     */
     public String getBehindDoor(){
         return behindDoor;
     }
 
+    /**
+     * Setter for the behindDoor variable
+     * @param behindDoor The new string of what is behind the door
+     */
     public void setBehindDoor(String behindDoor){
         this.behindDoor = behindDoor;
     }
