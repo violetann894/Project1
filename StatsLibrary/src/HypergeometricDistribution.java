f0deb30d49d6ce4f09de050ffd6b1213bc85ce2d
violetann894 <rachelhuss2976@gmail.com>
2025-03-04 19:14:47 -0500

    Created outline of hypergeometric distribution class

---
+++ StatsLibrary/src/HypergeometricDistribution.java
@@ -0,0 +1,49 @@
+/**
+ *
+ */
+public class HypergeometricDistribution {
+
+    /**
+     *
+     * @param N
+     * @param n
+     * @param y
+     * @param r
+     * @return
+     */
+    public double hypergeometricDistribution(int N, int n, int y, int r){
+
+    }
+
+    /**
+     *
+     * @param n
+     * @param r
+     * @param N
+     * @return
+     */
+    public double expectedValue(int n, int r, int N){
+
+    }
+
+    /**
+     *
+     * @param n
+     * @param r
+     * @param N
+     * @return
+     */
+    public double variance(int n, int r, int N){
+
+    }
+
+    /**
+     *
+     * @param variance
+     * @return
+     */
+    public double standardDeviation(double variance){
+
+    }
+
+}
